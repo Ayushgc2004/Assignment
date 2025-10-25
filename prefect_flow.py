58f0c4902117695ec7657f9ff12bbb3166922e2b
Ayushgc2004 <136146665+Ayushgc2004@users.noreply.github.com>
2025-10-25 23:38:22 +0530

    Add files via upload

---
+++ prefect_flow.py
@@ -0,0 +1,39 @@
+from prefect import flow, task
+from datetime import timedelta
+from etl_tasks import (
+    read_data_task, merge_data_task, load_to_postgres_task,
+    create_sales_summary_task, create_delivery_summary_task,
+    FILE_CUSTOMERS, FILE_ORDERS, FILE_ITEMS, FILE_PAYMENTS
+)
+
+@flow(name="Olist Hourly Data Pipeline")
+def olist_etl_pipeline():
+    # 1. Extraction/Cleaning
+    df_cust = read_data_task.submit(FILE_CUSTOMERS)
+    df_orders = read_data_task.submit(FILE_ORDERS)
+    df_items = read_data_task.submit(FILE_ITEMS)
+    df_payments = read_data_task.submit(FILE_PAYMENTS)
+
+    # 2. Merging (waits for all reads to complete)
+    df_merged = merge_data_task.submit(df_cust, df_orders, df_items, df_payments)
+
+    # 3. Load Staging
+    load_to_postgres_task.submit(df_merged, 'staging_olist_data')
+
+    # 4. Create Analytical Tables
+    df_sales = create_sales_summary_task.submit(df_merged)
+    df_delivery = create_delivery_summary_task.submit(df_merged)
+
+    # 5. Load Analytical Tables
+    load_to_postgres_task.submit(df_sales, 'analytics_sales_summary')
+    load_to_postgres_task.submit(df_delivery, 'analytics_delivery_performance')
+
+    print("✅ Pipeline run complete. Data loaded into PostgreSQL.")
+
+
+if __name__ == "__main__":
+    olist_etl_pipeline.serve(
+        name="hourly-olist-etl",
+        schedules=[timedelta(hours=1)], 
+        work_pool_name="default-agent-pool"  
+    )
